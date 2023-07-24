ca05c4f658cb0f6b0506ac51f7940596499d5e2c
vrajdesai78 <vrajdesai78@gmail.com>
2023-07-24 17:57:56 +0530

    added contract integration

---
+++ src/utils/viemClient.ts
@@ -0,0 +1,7 @@
+import { createPublicClient, http } from 'viem'
+import { polygonMumbai } from 'viem/chains'
+
+export const publicClient = createPublicClient({ 
+  chain: polygonMumbai,
+  transport: http()
+})
